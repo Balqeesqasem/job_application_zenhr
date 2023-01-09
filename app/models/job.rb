5c656dc89aabbba9c237afcd05c1ca56db54b6fe
balqeesqasem <balqeesmohammadq.95@gmail.com>
2023-01-09 23:10:57 +0300

    Add Filtering and add comments

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,22 @@
 class Job < ApplicationRecord
+  # Constant
   JOB_TYPES = [FULL_TIME = "full_time", PART_TIME = "part_time"]
+
+  # Associations
+  has_many :job_applications, dependent: :destroy
+
+  # Validations
   validates :job_title, :job_description, :company_name ,presence: true
 
+  # Enum
   enum job_type: JOB_TYPES
+
+  # Filtering attributes
+  concerning :Filtering do
+    class_methods do
+      def filterable_attributes
+        ["job_title", "created_at"]
+      end
+    end
+  end
 end
