6e9479ae6cd7ecf2378d52fc2cc6ae65713e2c49
balqeesqasem <balqeesmohammadq.95@gmail.com>
2023-01-09 18:45:03 +0300

    Add update method and route

--- app/controllers/v1/jobs_controller.rb
+++ app/controllers/v1/jobs_controller.rb
@@ -12,10 +12,23 @@ class V1::JobsController <  V1::ApplicationController
     end
   end
 
+  def update
+    job = Job.find params[:id]
+    if job.update(job_params)
+      render json: JobSerializer.render(job)
+    else
+      render json: {error: job.errors.full_messages}, status: :unauthorized
+    end
+  end
+
   # /v1/jobs , method: get
   def index
     jobs = Job.all
     render json: JobSerializer.render(jobs)
+  end
+
+  def job_params
+    params.permit(:job_title, :job_type, :company_name, :job_description)
   end
 
 end
