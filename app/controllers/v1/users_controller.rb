82bd5422855ac356aed6290a27ecf30fd78c1479
balqeesqasem <balqeesmohammadq.95@gmail.com>
2023-01-09 18:14:58 +0300

    Add user controller to v1 file

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,7 +1,7 @@
 
-class UsersController < ApplicationController
+class V1::UsersController <  V1::ApplicationController
 
-  # /users/current
+  # /v1/current
   # For this endpoint you should send client, uid and access token with headers
   def current
     render json: UserSerializer.render(current_user)
